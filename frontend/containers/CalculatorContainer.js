a6645f510b46d8378c2cf8f7e6a98436a2ffec57
Wangjun Yang <Wangjun.Yang@derivco.ee>
2020-03-13 12:35:44 +0200

    Finish calculator

--- frontend/containers/CalculatorContainer.js
+++ frontend/containers/CalculatorContainer.js
@@ -1,56 +1,79 @@
 import React, { Component } from 'react';
 
+const isPrime = num => {
+    for(let i = 2; i < num; i++)
+        if(num % i === 0) return false;
+    return num > 1;
+};
+
+const getLargestPrimeNumber = (startNumber,endNumber) => {
+
+    if (endNumber <= startNumber) {
+        alert('The end number should be higher than start number')
+    }
+
+    let primeNumbers = [];
+
+    for(let i = Math.floor(startNumber); i < Math.floor(endNumber); i++){
+        if (isPrime(i)) {
+            primeNumbers.push(i)
+        }
+    }
+    return Math.max(...primeNumbers);
+};
+
+const CalculatorOperations = {
+    '÷': (prevValue, nextValue) => prevValue / nextValue,
+    'x': (prevValue, nextValue) => prevValue * nextValue,
+    '+': (prevValue, nextValue) => prevValue + nextValue,
+    '-': (prevValue, nextValue) => prevValue - nextValue,
+    '%': (prevValue, nextValue) => prevValue % nextValue,
+    'P': getLargestPrimeNumber,
+    '=': (prevValue, nextValue) => nextValue
+};
+
+
 
 class CalculatorContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {
             displayValue: '0',
-            waitingForOperand: false,
-            result: null
+            operator: null,
+            primeNumberResult: null,
         };
     }
 
     inputDigit(digit) {
-        const { displayValue, waitingForOperand } = this.state;
-
-        if (waitingForOperand) {
-            this.setState({
-                displayValue: displayValue + digit,
-                waitingForOperand: false,
-            });
-        } else {
-            this.setState({
-                displayValue: displayValue === '0' ? String(digit) : displayValue + digit,
-            });
-        }
+        const { displayValue } = this.state;
+        this.setState({
+            displayValue: displayValue === '0' ? String(digit) : displayValue + digit,
+        });
     }
 
-    inputOperator(operator) {
-        const { displayValue, waitingForOperand } = this.state;
+    inputOperator(value) {
+        const { displayValue,operator } = this.state;
         let lastDigitRegexp = /\d$/;
         let operatorRegexp = /[x÷\-+]$/;
-        if (displayValue === '0') {
+        if (displayValue === '0' || operator) {
             return
         }
         else if (lastDigitRegexp.test(displayValue)){
             this.setState({
-                displayValue: displayValue + operator,
-                waitingForOperand: true
+                displayValue: displayValue + value,
+                operator: value
             });
         }
         else if (operatorRegexp.test(displayValue)) {
-            console.log('test');
             this.setState({
-                displayValue: displayValue.replace(operatorRegexp, operator),
-                waitingForOperand: true
+                displayValue: displayValue.replace(operatorRegexp, value),
+                operator: value
             });
         }
     }
 
     inputDot() {
         const { displayValue } = this.state;
-
         if (!(/\./).test(displayValue)) {
             this.setState({
                 displayValue: displayValue + '.',
@@ -59,33 +82,8 @@ class CalculatorContainer extends Component {
         }
     }
 
-    inputPercent() {
-        const { displayValue } = this.state;
-        const currentValue = parseFloat(displayValue);
-
-        if (currentValue === 0)
-            return;
-
-        const fixedDigits = displayValue.replace(/^-?\d*\.?/, '');
-        const newValue = parseFloat(displayValue) / 100;
-
-        this.setState({
-            displayValue: String(newValue.toFixed(fixedDigits.length + 2)),
-        });
-    }
-
-    toggleSign() {
-        const { displayValue } = this.state;
-        const newValue = parseFloat(displayValue) * -1;
-
-        this.setState({
-            displayValue: String(newValue),
-        });
-    }
-
     clearLastChar() {
         const { displayValue } = this.state;
-
         this.setState({
             displayValue: displayValue.substring(0, displayValue.length - 1) || '0'
         })
@@ -94,18 +92,28 @@ class CalculatorContainer extends Component {
     clearAll() {
         this.setState({
             displayValue: '0',
-            waitingForOperand: false,
-            result: null
+            operator: null,
         })
     }
 
-    calculate(displayValue) {
+    calculate() {
+        const { displayValue,operator } = this.state;
+        const regex = /\d+(\.\d+)?/g;
 
+        const matches = displayValue.match(regex);
+        const firstValue = parseFloat(matches[0]);
+        const secondValue =  parseFloat(matches[1]);
+
+        this.setState({
+            displayValue:  String(CalculatorOperations[operator](firstValue,secondValue)),
+            operator: null,
+        });
     }
 
     render() {
         return (<div className="container">
             <h2 style={ { margin: 40, textAlign: 'center' } }>Simple calculator</h2>
+            <p style={ { margin: 40, textAlign: 'center' } }>Below "P" is the operator to get the largest prime number between A and B,  for example, 1P9  return largest prime number 7 between 1 and 9</p>
             <table style={ { border: '1px solid #ddd', textAlign: 'center', width: '50%', margin: '0 auto' } }>
                 <tbody>
                 <tr>
@@ -114,13 +122,6 @@ class CalculatorContainer extends Component {
                         textAlign: 'right',
                         padding: 20,
                     } }>{ this.state.displayValue }</th>
-                </tr>
-                <tr>
-                    <th colSpan="4" style={ {
-                        border: '1px solid #ddd',
-                        textAlign: 'right',
-                        padding: 20,
-                    } }>{ this.state.result }</th>
                 </tr>
                 <tr>
                     <th colSpan="4" style={ { border: '1px solid #ddd', textAlign: 'right', padding: 5 } }>
@@ -133,34 +134,26 @@ class CalculatorContainer extends Component {
                     </th>
                 </tr>
                 <tr>
-                    <td style={ { border: '1px solid #ddd', textAlign: 'center', padding: 20 } }>
+                    <td colSpan="2" style={ { border: '1px solid #ddd', textAlign: 'center'} }>
                         <button
-                            className="btn"
-                            style={ { margin: 20 } }
+                            className="btn btn-lg"
+                            style={ { width:'100%' } }
                             onClick={ () => this.clearAll() }>
                             C
                         </button>
                     </td>
-                    <td style={ { border: '1px solid #ddd', textAlign: 'center', padding: 20 } }>
+                    <td style={ { border: '1px solid #ddd', textAlign: 'center'} }>
                         <button
                             className="btn"
-                            style={ { margin: 20 } }
-                            onClick={ () => this.toggleSign() }>
-                            +/-
-                        </button>
-                    </td>
-                    <td style={ { border: '1px solid #ddd', textAlign: 'center', padding: 20 } }>
-                        <button
-                            className="btn"
-                            style={ { margin: 20 } }
-                            onClick={ () => this.inputPercent() }>
+                            style={ { width:'100%' } }
+                            onClick={ () => this.inputOperator('%') }>
                             %
                         </button>
                     </td>
-                    <td style={ { border: '1px solid #ddd', textAlign: 'center', padding: 20 } }>
+                    <td style={ { border: '1px solid #ddd', textAlign: 'center'} }>
                         <button
                             className="btn"
-                            style={ { margin: 20 } }
+                            style={ { width:'100%' } }
                             onClick={ () => this.inputOperator('÷') }>
                             ÷
                         </button>
@@ -170,7 +163,7 @@ class CalculatorContainer extends Component {
                     <td style={ { border: '1px solid #ddd', textAlign: 'center', padding: 20 } }>
                         <button
                             className="btn"
-                            style={ { margin: 20 } }
+                            style={ { width:'100%' } }
                             onClick={ () => this.inputDigit(7) }>
                             7
                         </button>
@@ -178,7 +171,7 @@ class CalculatorContainer extends Component {
                     <td style={ { border: '1px solid #ddd', textAlign: 'center', padding: 20 } }>
                         <button
                             className="btn"
-                            style={ { margin: 20 } }
+                            style={ { width:'100%' } }
                             onClick={ () => this.inputDigit(8) }>
                             8
                         </button>
@@ -186,7 +179,7 @@ class CalculatorContainer extends Component {
                     <td style={ { border: '1px solid #ddd', textAlign: 'center', padding: 20 } }>
                         <button
                             className="btn"
-                            style={ { margin: 20 } }
+                            style={ { width:'100%' } }
                             onClick={ () => this.inputDigit(9) }>
                             9
                         </button>
@@ -194,7 +187,7 @@ class CalculatorContainer extends Component {
                     <td style={ { border: '1px solid #ddd', textAlign: 'center', padding: 20 } }>
                         <button
                             className="btn"
-                            style={ { margin: 20 } }
+                            style={ { width:'100%' } }
                             onClick={ () => this.inputOperator('x') }>
                             x
                         </button>
@@ -204,7 +197,7 @@ class CalculatorContainer extends Component {
                     <td style={ { border: '1px solid #ddd', textAlign: 'center', padding: 20 } }>
                         <button
                             className="btn"
-                            style={ { margin: 20 } }
+                            style={ { width:'100%' } }
                             onClick={ () => this.inputDigit(4) }>
                             4
                         </button>
@@ -212,7 +205,7 @@ class CalculatorContainer extends Component {
                     <td style={ { border: '1px solid #ddd', textAlign: 'center', padding: 20 } }>
                         <button
                             className="btn"
-                            style={ { margin: 20 } }
+                            style={ { width:'100%' } }
                             onClick={ () => this.inputDigit(5) }>
                             5
                         </button>
@@ -220,7 +213,7 @@ class CalculatorContainer extends Component {
                     <td style={ { border: '1px solid #ddd', textAlign: 'center', padding: 20 } }>
                         <button
                             className="btn"
-                            style={ { margin: 20 } }
+                            style={ { width:'100%' } }
                             onClick={ () => this.inputDigit(6) }>
                             6
                         </button>
@@ -228,7 +221,7 @@ class CalculatorContainer extends Component {
                     <td style={ { border: '1px solid #ddd', textAlign: 'center', padding: 20 } }>
                         <button
                             className="btn"
-                            style={ { margin: 20 } }
+                            style={ { width:'100%' } }
                             onClick={ () => this.inputOperator('-') }>
                             -
                         </button>
@@ -238,7 +231,7 @@ class CalculatorContainer extends Component {
                     <td style={ { border: '1px solid #ddd', textAlign: 'center', padding: 20 } }>
                         <button
                             className="btn"
-                            style={ { margin: 20 } }
+                            style={ { width:'100%' } }
                             onClick={ () => this.inputDigit(1) }>
                             1
                         </button>
@@ -246,7 +239,7 @@ class CalculatorContainer extends Component {
                     <td style={ { border: '1px solid #ddd', textAlign: 'center', padding: 20 } }>
                         <button
                             className="btn"
-                            style={ { margin: 20 } }
+                            style={ { width:'100%' } }
                             onClick={ () => this.inputDigit(2) }>
                             2
                         </button>
@@ -254,15 +247,15 @@ class CalculatorContainer extends Component {
                     <td style={ { border: '1px solid #ddd', textAlign: 'center', padding: 20 } }>
                         <button
                             className="btn"
-                            style={ { margin: 20 } }
-                            onClick={ () => this.inputDigit(4) }>
+                            style={ { width:'100%' } }
+                            onClick={ () => this.inputDigit(3) }>
                             3
                         </button>
                     </td>
                     <td style={ { border: '1px solid #ddd', textAlign: 'center', padding: 20 } }>
                         <button
                             className="btn"
-                            style={ { margin: 20 } }
+                            style={ { width:'100%' } }
                             onClick={ () => this.inputOperator('+') }>
                             +
                         </button>
@@ -272,15 +265,15 @@ class CalculatorContainer extends Component {
                     <td style={ { border: '1px solid #ddd', textAlign: 'center', padding: 20 } }>
                         <button
                             className="btn"
-                            style={ { margin: 20 } }
-                            onClick={ this.handleClick }>
-                            ()
+                            style={ { width:'100%' } }
+                            onClick={ () => this.inputOperator('P') }>
+                            P
                         </button>
                     </td>
                     <td style={ { border: '1px solid #ddd', textAlign: 'center', padding: 20 } }>
                         <button
                             className="btn"
-                            style={ { margin: 20 } }
+                            style={ { width:'100%' } }
                             onClick={ () => this.inputDigit(0) }>
                             0
                         </button>
@@ -288,7 +281,7 @@ class CalculatorContainer extends Component {
                     <td style={ { border: '1px solid #ddd', textAlign: 'center', padding: 20 } }>
                         <button
                             className="btn"
-                            style={ { margin: 20 } }
+                            style={ { width:'100%' } }
                             onClick={ () => this.inputDot() }>
                             .
                         </button>
@@ -296,8 +289,8 @@ class CalculatorContainer extends Component {
                     <td style={ { border: '1px solid #ddd', textAlign: 'center', padding: 20 } }>
                         <button
                             className="btn"
-                            style={ { margin: 20 } }
-                            onClick={ this.handleClick }>
+                            style={ { width:'100%' } }
+                            onClick={ () => this.calculate() }>
                             =
                         </button>
                     </td>
